53207f47133bca83fddab4f250ed6a4b5c124574
pavel-agarkov <pavel.agarkov@gmail.com>
2026-02-15 16:17:59 +0100

    add pulumi project

---
+++ LiveArch.Deployment/Program.cs
@@ -0,0 +1,28 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+using System.Collections.Generic;
+
+return await Pulumi.Deployment.RunAsync(() =>
+{
+    // Create an Azure Resource Group
+    var resourceGroup = new ResourceGroup("resourceGroup");
+
+    // Create an Azure Storage Account
+    var storageAccount = new StorageAccount("sa", new StorageAccountArgs
+    {
+        ResourceGroupName = resourceGroup.Name,
+        Sku = new SkuArgs
+        {
+            Name = SkuName.Standard_LRS
+        },
+        Kind = Kind.StorageV2
+    });
+
+    // Export the storage account name
+    return new Dictionary<string, object?>
+    {
+        ["storageAccountName"] = storageAccount.Name
+    };
+});
