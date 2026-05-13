408246bc763d95514024b5850ca9a3f01a5b1f21
Pavel Agarkov <pavel.agarkov@de.abb.com>
2026-05-13 16:55:12 +0200

    removing open api

--- LiveArch.Order.Api/Program.cs
+++ LiveArch.Order.Api/Program.cs
@@ -10,15 +10,12 @@ namespace LiveArch.Order.Api
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
             }
 
             app.UseHttpsRedirection();
